d18ec14f9acc25e51e442bcec418627c6524d2fe
acadmyn <71316892+acadmyn@users.noreply.github.com>
2020-09-15 13:00:28 +0200

    Update dcapulsepi.bash
    Lösenordsbyte och kräv lösenord för sudo

--- dcapulsepi.bash
+++ dcapulsepi.bash
@@ -1,23 +1,31 @@
-echo "Updating..."
+echo "Byt standardlösen (raspberry)"
+passwd
+
+echo "Skapa ett lösen för rootanvändaren: "
+sudo passwd
+
+sed -i 's/NOPASSWD/PASSWD/' /etc/sudoers.d/010_pi-nopasswd
+
+echo "Uppdaterar..."
 apt update -y
 apt upgrade -y
-echo "Done updating. Waiting a few seconds..."
+echo "Klar. Väntar ett par sekunder..."
 sleep 5
 clear
 
-echo "Installing desktop environment..."
+echo "Installerar skrivbordsmiljö..."
 apt install -y raspberrypi-ui-mods
-echo "Done installing DE. Waiting a few seconds..."
+echo "Klar. Väntar ett par sekunder..."
 sleep 5
 clear
 
-echo "Installing Firefox..."
+echo "Installerar Firefox..."
 apt install -y firefox-esr
-echo "Done installing Firefox. Waiting a few seconds..."
+echo "Klar. Väntar ett par sekunder..."
 sleep 5
 clear
 
-echo "Installing Teamviewer..."
+echo "Installerar Teamviewer..."
 cd /home/pi
 wget https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb
 dpkg -i teamviewer-host_armhf.deb
@@ -27,24 +35,24 @@ echo "Välj ett lösenord för TeamViewer host: "
 read twpasswd
 teamviewer passwd $twpasswd
 teamviewer license accept
-echo "Done installing Teamviewer. Waiting a few seconds..."
+echo "Klar. Väntar ett par sekunder..."
 sleep 5
 
-echo "Installing Log2RAM..."
+echo "Installerar Log2RAM..."
 apt install -y git
 cd /home/pi
 git clone https://github.com/azlux/log2ram.git
 cd log2ram
 chmod +x install.sh
 sh ./install.sh
-echo "Done installing Log2RAM. Waiting a few seconds..."
+echo "Klar. Väntar ett par sekunder..."
 sleep 5
 
-echo "Creating daily reboot crontab"
-crontab -l | { cat; echo "0 0	* * *	root	/sbin/shutdown -r now"; } | crontab -
+#echo "Skapar schemalagd omstart..."
+#crontab -l | { cat; echo "0 0	* * *	root	/sbin/shutdown -r now"; } | crontab -
 
 
-echo "Cleaning up a little bit..!"
+echo "Städar upp lite..."
 apt autoremove -y
 
 reboot now
